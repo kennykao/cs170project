99ecf3360824215e28b5021f402e29305110d7f6
kennykao <kennethkao32@berkeley.edu>
2017-04-28 14:36:31 -0700

    okay time to try

--- sample_solver.py
+++ sample_solver.py
@@ -9,26 +9,10 @@ import argparse
   Please complete the following function.
 ===============================================================================
 """
-def alg3(P, M, N, C, items, constraints):
-  sorted_by_second = sorted(items, key=lambda tup: tup[5])
-  print(sorted_by_second)
-  i = N - 1
-  sack = [P,M]
-  insack = []
-  while i > 0:
-    if sack[0] == 0: 
-      break
-    elif 
-      insack.append(items[i])
-      i -= 1
-
-
-
 
 def solve(P, M, N, C, items, constraints):
   """
   Write your amazing algorithm here.
-
   Return: a list of strings, corresponding to item names.
   """
   '''
@@ -37,30 +21,230 @@ def solve(P, M, N, C, items, constraints):
   if resale < cost: dont buy
   base cost off of resale - cost; always have a positive value
   '''
-  d = dict()
+
+  # INITIALIZING DATA STRUCTURES
+  
+  d = dict()                  # Dictionary of Constraints { item_class : {constraints set} }
+  item_val_lst = []           # List of Item Net Values  [ (resale value - cost, item_name) ]
+  item_weight_lst = []        # List of Item Weights [ (weight, item_name) ]
+  item_ratio_lst = []
+  item_combined = []
+  item_cost_val_dict = dict() # Dictionary of Items { item_name : (cost, resale value, weight, class) }
+
   i = 0
-  while i<N:
+  while i < N:
+    curr_item = items[i]
     d[i] = set()
+    if (curr_item[4] - curr_item[3] <= 0):
+      i += 1
+      continue
+    item_val_lst.append( (curr_item[4] - curr_item[3], curr_item[0]) )
+    item_weight_lst.append( (curr_item[2], curr_item[0]) )
+    item_ratio_lst.append(((curr_item[4]-curr_item[3])/curr_item[2], curr_item[0]))
+    item_combined.append((curr_item[4] - curr_item[3], curr_item[2], curr_item[0]))
+    item_cost_val_dict[curr_item[0]] = (curr_item[3], curr_item[4], curr_item[2], curr_item[1]) 
     i += 1
+ 
   for constraint in constraints:
     for i in constraint:
       d[i] = d[i].union(constraint)
       d[i].remove(i)
-  i = 0
-  while i < N:
-    if items[i][3] > items[i][4]:
-      del items[i]
-      i += 1
-    else:
-      items[i] = items[i] + (items[i][4] - items[i][3],)
-      print(items[i])
-      i += 1
+  i = 0 
 
-  alg3(P,M,N,C,items,constraints)
+  print('cool')
 
 
+  # MAX VALUE GREEDY ALGORITHM
+  # Choose the item that has the maximum value from the remaining items
+  def max_val_greedy(P, M, lst):
+    lst = lst[:]
+    lst_classes = set()
+    result = []
+    total_resale = 0
+
+    while (lst):
+      lst.sort(reverse = True)
+      curr_max = lst[0]
+      curr_max_cost = item_cost_val_dict[curr_max[1]][0]
+      curr_max_weight = item_cost_val_dict[curr_max[1]][2]
+      curr_max_class = item_cost_val_dict[curr_max[1]][3]
+      if (M == 0) or (P == 0):
+        break
+      elif (curr_max_cost <= M) and (curr_max_weight <= P) and (curr_max_class not in lst_classes):
+        result.append(curr_max[1])
+        for c in d[curr_max_class]:
+          lst_classes.add(c)
+        total_resale += curr_max[0]
+        lst.remove(curr_max)
+        M = M - curr_max_cost 
+        P = P - curr_max_weight
+      else:
+        lst.remove(curr_max)
+
+    return (total_resale, result)
 
 
+  # MIN WEIGHT GREEDY ALGORITHM
+  # Choose the lightest item from the remaining items
+  def min_weight_greedy(P, M, lst):
+    print('hit')
+    lst = lst[:]
+    lst_classes = set()
+    result = []
+    total_resale = 0
+
+    while (lst):
+      curr_min = min(lst)
+      curr_min_cost = item_cost_val_dict[curr_min[1]][0]
+      curr_min_weight = item_cost_val_dict[curr_min[1]][2]
+      curr_min_class = item_cost_val_dict[curr_min[1]][3]
+
+      if (M == 0) or (P == 0):
+        break
+      elif (curr_min_cost <= M) and (curr_min_weight <= P) and (curr_min_class not in lst_classes):
+        result.append(curr_min[1])
+        for c in d[curr_min_class]:
+          lst_classes.add(c)
+        total_resale += item_cost_val_dict[curr_min[1]][1]
+        lst.remove(curr_min)
+        M = M - curr_min_cost 
+        P = P - curr_min_weight 
+      else:
+        lst.remove(curr_min)
+
+    return (total_resale, result)
+
+
+  # VALUE TO WEIGHT RATION GREEDY ALGORITHM
+  # Choose the items with as high a value per weight as possible
+  def val_to_weight_greedy(P, M, lst):
+    lst = lst[:]
+    lst_classes = set()
+    result = []
+    total_resale = 0
+
+    while (lst):
+      curr_max = max(lst)
+      curr_max_cost = item_cost_val_dict[curr_max[1]][0]
+      curr_max_weight = item_cost_val_dict[curr_max[1]][2]
+      curr_max_class = item_cost_val_dict[curr_max[1]][3]
+      if (M == 0) or (P == 0):
+        break
+      elif (curr_max_cost <= M) and (curr_max_weight <= P) and (curr_max_class not in lst_classes):
+        result.append(curr_max[1])
+        for c in d[curr_max_class]:
+          lst_classes.add(c)
+        total_resale += curr_max[0]
+        lst.remove(curr_max)
+        M = M - curr_max_cost 
+        P = P - curr_max_weight
+      else:
+        lst.remove(curr_max)
+    return (total_resale, result)
+
+  # HYBRID GREEDY ALGORITHM
+  # Choose max value until 90% full, then switch to min weight. Then sweep down from 90% - ~50%? 
+  def hybrid_greedy(P, M, lst):
+    lst = lst[:]
+    lst_classes = set()
+    result = []
+    total_resale = 0
+
+    reduced_P = P * 0.7
+    reduced_P2 = P - reduced_P 
+       
+    while (lst):
+      curr_max = max(lst)
+      curr_max_cost = item_cost_val_dict[curr_max[2]][0]
+      curr_max_weight = item_cost_val_dict[curr_max[2]][2]
+      curr_max_class = item_cost_val_dict[curr_max[2]][3]
+      if (M == 0) or (reduced_P == 0):
+        break
+      elif (curr_max_cost <= M) and (curr_max_weight <= reduced_P) and (curr_max_class not in lst_classes):
+        result.append(curr_max[2])
+        for c in d[curr_max_class]:
+          lst_classes.add(c)
+        total_resale += item_cost_val_dict[curr_max[2]][0]
+        lst.remove(curr_max)
+        M = M - curr_max_cost 
+        reduced_P = reduced_P - curr_max_weight
+      else:
+        lst.remove(curr_max)
+    
+    while (lst):
+      lst.sort(key=lambda x: x[1])
+      curr_min = min(lst)
+      curr_min_cost = item_cost_val_dict[curr_min[2]][0]
+      curr_min_weight = item_cost_val_dict[curr_min[2]][2]
+      curr_min_class = item_cost_val_dict[curr_min[2]][3]
+      if (M == 0) or (reduced_P2 == 0):
+        break
+      elif (curr_min_cost <= M) and (curr_min_weight <= reduced_P2) and (curr_min_class not in lst_classes):
+        result.append(curr_min[2])
+        for c in d[curr_min_class]:
+          lst_classes.add(c)
+        total_resale += item_cost_val_dict[curr_min[2]][0]
+        lst.remove(curr_min)
+        M = M - curr_min_cost 
+        reduced_P2 = reduced_P2 - curr_min_weight
+      else:
+        lst.remove(curr_min)        
+    return (total_resale, result)
+
+  def hybrid_greedy2(P, M, lst):
+    lst = lst[:]
+    lst_classes = set()
+    result = []
+    total_resale = 0
+
+    reduced_P = P * 0.7
+    reduced_P2 = P - reduced_P
+    while (lst):
+      curr_max = max(lst)
+      curr_max_cost = item_cost_val_dict[curr_max[2]][0]
+      curr_max_weight = item_cost_val_dict[curr_max[2]][2]
+      curr_max_class = item_cost_val_dict[curr_max[2]][3]
+      if (M == 0) or (reduced_P == 0):
+        break
+      elif (curr_max_cost <= M) and (curr_max_weight <= reduced_P) and (curr_max_class not in lst_classes):
+        result.append(curr_max[2])
+        for c in d[curr_max_class]:
+          lst_classes.add(c)
+        total_resale += curr_max[0]
+        lst.remove(curr_max)
+        M = M - curr_max_cost 
+        reduced_P = reduced_P - curr_max_weight
+      else:
+        lst.remove(curr_max)
+
+    while (lst):
+      curr_min = min(lst)
+      print(curr_min[1])
+      curr_min_cost = item_cost_val_dict[curr_min[1]][0]
+      curr_min_weight = item_cost_val_dict[curr_min[1]][2]
+      curr_min_class = item_cost_val_dict[curr_min[1]][3]
+      if (M == 0) or (reduced_P2 == 0):
+        break
+      elif (curr_min_cost <= M) and (curr_min_weight <= reduced_P2) and (curr_min_class not in lst_classes):
+        result.append(curr_min[1])
+        for c in d[curr_min_class]:
+          lst_classes.add(c)
+        total_resale += item_cost_val_dict[curr_min[1]][1]
+        lst.remove(curr_min)
+        M = M - curr_min_cost 
+        reduced_P2 = reduced_P2 - curr_max_weight
+      else:
+        lst.remove(curr_min)        
+    return (total_resale, result)
+
+
+  # Take max of all algorithms
+  alg_one = max_val_greedy(P, M, item_val_lst)
+  alg_two = min_weight_greedy(P, M, item_weight_lst)
+  alg_three = val_to_weight_greedy(P,M,item_ratio_lst)
+  alg_four = hybrid_greedy(P,M,item_combined)
+
+  return max([alg_one, alg_two, alg_three, alg_four])[1]
 
 """
 ===============================================================================
