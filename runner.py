43c2375cd0e37a2a7dbbc78fc5db14fe1d3e5212
Christine Munar <chrismunar@berkeley.edu>
2017-04-29 14:04:10 -0700

    All output files except 14. Added team name too

--- runner.py
+++ runner.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 
 if __name__ == "__main__":
-	a = 16
-	b = 16
+	a = 18
+	b = 18
 
 	while a < 22:
-		# os.system("python sample_solver.py /Users/christinemunar/Documents/Spring2017/CS170/PhaseII-Proj/cs170project/project_instances/problem" + str(a) + ".in" + " wow" + str(b) + ".out")
-		os.system("python sample_solver.py /Users/KennyKao1/Desktop/cs170proj/project_instances/problem" + str(a) + ".in" + " wow" + str(b) + ".out")
+		os.system("python sample_solver.py /Users/christinemunar/Documents/Spring2017/CS170/PhaseII-Proj/cs170project/project_instances/problem" + str(a) + ".in" + " wow" + str(b) + ".out")
+		# os.system("python sample_solver.py /Users/KennyKao1/Desktop/cs170proj/project_instances/problem" + str(a) + ".in" + " wow" + str(b) + ".out")
 		a += 1
 		b += 1
