8a6de6bf14c0f3d41f3a8b064e665b00f003ea84
kennykao <kennethkao32@berkeley.edu>
2017-04-28 23:24:04 -0700

    1-13 done

--- runner.py
+++ runner.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 	a = 1
 	b = 1
 
-	while a < 22:
+	while a < 14:
 		os.system("python sample_solver.py /Users/KennyKao1/Desktop/cs170proj/project_instances/problem" + str(a) + ".in" + " wow" + str(b) + ".out")
 		a += 1
 		b += 1
